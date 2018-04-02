c9f1cddf74aea1bafc21fae2756d17fdf3ed56bb
邱梦孟 <1790338342@qq.com>
2018-04-02 15:07:26 +0800

    page-1-首页-轮播图

--- src/js/page-1/entry-index.js
+++ src/js/page-1/entry-index.js
@@ -1,9 +1,18 @@
 // 1-课程列表页-入口
 define([
-	"publicBusiness/layerFz"
+	"publicBusiness/layerFz",
+	"TouchSlide",
 ], function (layer) {
 	layer.msg({
 		content: "首页"
+	});
+	// 轮播图
+	TouchSlide({ 
+		slideCell:"#slideBox",
+		titCell:".hd ul li",
+		mainCell:".bd ul",
+		effect:"leftLoop",
+		autoPlay:true
 	});
 	return function(){
 		
